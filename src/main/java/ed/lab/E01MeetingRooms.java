3bd7f10544b6abca6ab38ff06ceb02f348595b65
getoyom <getoyom@correo.url.edu.gt>
2025-05-23 07:56:44 -0600

    Parcial Resuelto

--- src/main/java/ed/lab/E01MeetingRooms.java
+++ src/main/java/ed/lab/E01MeetingRooms.java
@@ -1,11 +1,23 @@
 package ed.lab;
 
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class E01MeetingRooms {
-
+    PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->a-b);
     public int minMeetingRooms(List<MeetingInterval> meetingIntervals) {
-        return -1;
+        //Ordenar de menor a mayor tiempo de inicio
+        meetingIntervals.sort((a, b) -> a.startTime() - b.startTime());
+        //Evaluar cada dato
+        for (MeetingInterval interval : meetingIntervals) {
+            //Si la pq no está vacía y el peek de la pq es <= que el tiempo de inicio del intevalo, sacarlo
+            if (!pq.isEmpty() && pq.peek() <= interval.startTime()) {
+                pq.poll();
+            }
+            //Si no se cumple, guardar el tiempo final de ese intevalo.
+            pq.offer(interval.endTime());
+        }
+        return pq.size();
     }
 
 }
--- src/main/java/ed/lab/E02Logger.java
+++ src/main/java/ed/lab/E02Logger.java
@@ -1,12 +1,24 @@
 package ed.lab;
 
-public class E02Logger {
+import java.util.HashMap;
+import java.util.Map;
 
+public class E02Logger {
+    Map<String, Integer> map;
     public E02Logger() {
+        map = new HashMap<>();
+
     }
 
     public boolean shouldPrintMessage(int timestamp, String message) {
-        return false;
+        //Si ya existe el message
+        if(map.containsKey(message)) {
+            //Devolver si el tiempo que se ha dado - el tiempo inicial del mensaje es >=10
+            return timestamp - map.get(message) >= 10;
+        }
+        //Si no, agregarlo al map
+        map.put(message, timestamp);
+        return true;
     }
 
 }
